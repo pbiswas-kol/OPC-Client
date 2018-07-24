4513c257f1e0984ebb05b91c1e24069c3f4c9fb9
palashb <palash.biswas@yahoo.com>
2018-07-24 10:04:26 +0530

    Initial Commit

---
+++ OpcClientConsole/OpcClient.cs
@@ -0,0 +1,133 @@
+﻿using Opc.Ua;
+using Opc.Ua.Client;
+using Opc.Ua.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpcClientConsole
+{
+    public class POpcClient
+    {
+        const int ReconnectPeriod = 10;
+        Session session;
+        SessionReconnectHandler reconnectHandler;
+        string endpointURL;
+        static bool autoAccept = false;
+        public POpcClient(string _endpointURL, bool _autoAccept)
+        {
+            endpointURL = _endpointURL;
+            autoAccept = _autoAccept;
+        }
+        public async Task run()
+        {
+            ApplicationInstance application = new ApplicationInstance
+            {
+                ApplicationName = "UA Core Sample Client",
+                ApplicationType = ApplicationType.Client,
+                ConfigSectionName = "Opc.Ua.SampleClient"
+            };
+            ApplicationConfiguration config = await application.LoadApplicationConfiguration(false);
+            bool haveAppCertificate = await application.CheckApplicationInstanceCertificate(false, 0);
+            if (!haveAppCertificate)
+            {
+                throw new Exception("Application instance certificate invalid!");
+            }
+            if (haveAppCertificate)
+            {
+                config.ApplicationUri = Utils.GetApplicationUriFromCertificate(config.SecurityConfiguration.ApplicationCertificate.Certificate);
+                if (config.SecurityConfiguration.AutoAcceptUntrustedCertificates)
+                {
+                    autoAccept = true;
+                }
+                config.CertificateValidator.CertificateValidation += new CertificateValidationEventHandler(CertificateValidator_CertificateValidation);
+            }
+            else
+            {
+                Console.WriteLine("    WARN: missing application certificate, using unsecure connection.");
+            }
+
+            var selectedEndpoint = CoreClientUtils.SelectEndpoint(endpointURL, haveAppCertificate, 15000);
+            Console.WriteLine("    Selected endpoint uses: {0}", selectedEndpoint.SecurityPolicyUri.Substring(selectedEndpoint.SecurityPolicyUri.LastIndexOf('#') + 1));
+            var endpointConfiguration = EndpointConfiguration.Create(config);
+            var endpoint = new ConfiguredEndpoint(null, selectedEndpoint, endpointConfiguration);
+            session = await Session.Create(config, endpoint, false, "OPC UA Console Client", 60000, new UserIdentity(new AnonymousIdentityToken()), null);
+            session.KeepAlive += Client_KeepAlive;
+
+            var subscription = new Subscription(session.DefaultSubscription) { PublishingInterval = 1000 };
+            var list = new List<MonitoredItem> {
+                new MonitoredItem(subscription.DefaultItem)
+                {
+                    MonitoringMode = MonitoringMode.Reporting, NodeClass= NodeClass.Variable, SamplingInterval=1, QueueSize=1               //  DisplayName = "ServerStatusCurrentTime", StartNodeId = "i="+Variables.Server_ServerStatus_CurrentTime.ToString()
+                  ,DisplayName = "Temparature",  StartNodeId = "ns=5;s=Temparature"
+
+                },
+                 new MonitoredItem(subscription.DefaultItem)
+                {
+                    MonitoringMode = MonitoringMode.Reporting, NodeClass= NodeClass.Variable, SamplingInterval=1, QueueSize=1               //  DisplayName = "ServerStatusCurrentTime", StartNodeId = "i="+Variables.Server_ServerStatus_CurrentTime.ToString()
+                  ,DisplayName = "Humidity",  StartNodeId = "ns=5;s=Humidity"
+
+                }
+
+            };
+            list.ForEach(i => i.Notification += OnNotification);
+            subscription.AddItems(list);
+            session.AddSubscription(subscription);
+            subscription.Create();
+        }
+
+        private void OnNotification(MonitoredItem monitoredItem, MonitoredItemNotificationEventArgs e)
+        {
+            foreach (var value in monitoredItem.DequeueValues())
+            {
+                Console.WriteLine("{0}: {1}, {2}, {3}", monitoredItem.DisplayName, value.Value, value.SourceTimestamp, value.StatusCode);
+            }
+        }
+
+        private void Client_KeepAlive(Session sender, KeepAliveEventArgs e)
+        {
+            if (e.Status != null && ServiceResult.IsNotGood(e.Status))
+            {
+                Console.WriteLine("{0} {1}/{2}", e.Status, sender.OutstandingRequestCount, sender.DefunctRequestCount);
+
+                if (reconnectHandler == null)
+                {
+                    Console.WriteLine("--- RECONNECTING ---");
+                    reconnectHandler = new SessionReconnectHandler();
+                    reconnectHandler.BeginReconnect(sender, ReconnectPeriod * 1000, Client_ReconnectComplete);
+                }
+            }
+        }
+
+        private void Client_ReconnectComplete(object sender, EventArgs e)
+        {
+            if (!Object.ReferenceEquals(sender, reconnectHandler))
+            {
+                return;
+            }
+
+            session = reconnectHandler.Session;
+            reconnectHandler.Dispose();
+            reconnectHandler = null;
+            Console.WriteLine("--- RECONNECTED ---");
+        }
+
+        private void CertificateValidator_CertificateValidation(CertificateValidator sender, CertificateValidationEventArgs e)
+        {
+            if (e.Error.StatusCode == StatusCodes.BadCertificateUntrusted)
+            {
+                e.Accept = autoAccept;
+                if (autoAccept)
+                {
+                    Console.WriteLine("Accepted Certificate: {0}", e.Certificate.Subject);
+                }
+                else
+                {
+                    Console.WriteLine("Rejected Certificate: {0}", e.Certificate.Subject);
+                }
+            }
+        }
+    }
+}
---
+++ OpcClientConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using Opc.Ua;
+using Opc.Ua.Configuration;
+using System;
+using System.Threading;
+
+namespace OpcClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stopTimeout = Timeout.Infinite;
+            bool autoAccept = false;
+            string endpointURL = "opc.tcp://DESKTOP-GHG6DG6:53530/OPCUA/SimulationServer";
+            int clientRunTime = stopTimeout <= 0 ? Timeout.Infinite : stopTimeout * 1000;
+
+            POpcClient client = new POpcClient(endpointURL, autoAccept);
+            try
+            {
+                client.run().Wait(); 
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            ManualResetEvent quitEvent = new ManualResetEvent(false);
+            try
+            {
+                Console.CancelKeyPress += (sender, eArgs) =>
+                {
+                    quitEvent.Set();
+                    eArgs.Cancel = true;
+                };
+            }
+            catch
+            {
+            }
+
+            // wait for timeout or Ctrl-C
+            quitEvent.WaitOne(clientRunTime);
+
+        }
+    }
+}
